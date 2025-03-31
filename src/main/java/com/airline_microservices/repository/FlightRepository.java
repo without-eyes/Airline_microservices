c0115d9e7f7cb7560616d4710c7dfca73d8edc86
without eyes <37347347+without-eyes@users.noreply.github.com>
2025-03-31 19:12:55 +0300

    Delete src folder

--- src/main/java/com/airline_microservices/repository/FlightRepository.java
+++
@@ -1,8 +0,0 @@
-package com.airline_microservices.repository;
-
-import com.airline_microservices.model.Flight;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlightRepository extends JpaRepository<Flight, Long> {}
